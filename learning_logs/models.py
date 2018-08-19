fe2304215638ceab111d2f6a929206b1cf6d5a5b
Thomas Ngo <tngo0508@gmail.com>
2018-08-19 08:03:32 -0700

    mapping a url, writing a view

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,7 +9,7 @@ from django.db import models
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         """Return a string representation of the model."""
@@ -20,7 +20,7 @@ class Entry(models.Model):
     """Something specific learned about a topic."""
     topic = models.ForeignKey(Topic)
     text = models.TextField()
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'entries'
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """The home page for Learning Log"""
+    return render(request, 'learning_logs/index.html')
