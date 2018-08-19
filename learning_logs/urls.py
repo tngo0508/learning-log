75fdf9528bbbf78e10ed3135796ac625dc08d1be
Thomas Ngo <tngo0508@gmail.com>
2018-08-19 12:34:58 -0700

    add topic template and link everything together

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 
     # Show all topics.
     url(r'^topics/$', views.topics, name='topics'),
+    # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
