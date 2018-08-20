dd1be7be83815bc41f4a63426afc15a50f4f41af
Thomas Ngo <tngo0508@gmail.com>
2018-08-20 13:10:23 -0700

    associating new topics with the current user

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -47,7 +47,9 @@ def new_topic(request):
         # POST data submitted; process data
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
 
     context = {'form': form}
