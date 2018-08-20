3dedff0be95f4f7142d0e06420943169352087b5
Thomas Ngo <tngo0508@gmail.com>
2018-08-20 13:04:28 -0700

    protecting a user's topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,9 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
-# from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
@@ -29,6 +28,10 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
