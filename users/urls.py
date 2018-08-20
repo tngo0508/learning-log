065b1f3d1f87bc02d0a49782c4e765281e903ab2
Thomas Ngo <tngo0508@gmail.com>
2018-08-20 03:31:15 -0700

    logout_view() View function

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # Login page
     url(r'^login/$', login, {'template_name': 'users/login.html'},
         name='login'),
+    # Logout page
+    url(r'^logout/$', views.logout_view, name='logout'),
 ]
